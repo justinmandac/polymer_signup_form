e87a1c8915657daf9d737180de18a45314b34b11
Bien Justin Mandac <justinmandac@gmail.com>
2017-10-26 21:00:42 +0800

    Fixed typo on <signup-form> Polymer import. Wrapped <signup-form> JS in an IIFE

--- src/password-input/password-input.js
+++ src/password-input/password-input.js
@@ -1,27 +1,29 @@
-class PasswordInput extends Polymer.Element {
-  static get is() { return 'password-input'; }
-
-  static get properties() {
-    return {
-      required: {
-        type: Boolean,
-        value: false,
-        reflectToAttribute: true,
-      },
-
-      value: {
-        type: String,
-        value: '',
-      },
-
-      /** @private */
-      _passwordPair: {
-        type: String,
-        value: ''
-      },
-    };
+(() => {
+  class PasswordInput extends Polymer.Element {
+    static get is() { return 'password-input'; }
+  
+    static get properties() {
+      return {
+        required: {
+          type: Boolean,
+          value: false,
+          reflectToAttribute: true,
+        },
+  
+        value: {
+          type: String,
+          value: '',
+        },
+  
+        /** @private */
+        _passwordPair: {
+          type: String,
+          value: ''
+        },
+      };
+    }
   }
-}
-
-
-window.customElements.define(PasswordInput.is, PasswordInput);
+  
+  
+  window.customElements.define(PasswordInput.is, PasswordInput);
+})();
